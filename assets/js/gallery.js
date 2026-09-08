2ddf6733b764354b02f368c10d23b341a10e538e
imbush <52510653+imbush@users.noreply.github.com>
2026-09-08 19:55:41 +0000

    Deploying to gh-pages from @ imbush/imbush.github.io@ea6b4a75ce6cd90c9acd63817771e0f5fd9d5298 🚀

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,1 +1,14 @@
-document.addEventListener("DOMContentLoaded",function(){const e=document.getElementById("gallery-toggle"),n=document.querySelector(".gallery-container");e.addEventListener("click",()=>{const t=n.classList.toggle("single-column");console.log("Toggled:",t),e.src=t?'{{ "3column-icon-gray.png" | prepend: "assets/img/" | relative_url }}':'{{ "3column-icon-black.png" | prepend: "assets/img/" | relative_url }}'})});
+document.addEventListener('DOMContentLoaded', function() {
+    const galleryToggleBtn = document.getElementById("gallery-toggle");
+    const galleryContainer = document.querySelector(".gallery-container");
+
+    galleryToggleBtn.addEventListener("click", () => {
+        const isSingleColumn = galleryContainer.classList.toggle("single-column");
+    
+        console.log("Toggled:", isSingleColumn);
+        // Swap icon based on mode
+        galleryToggleBtn.src = isSingleColumn 
+            ? '{{ "3column-icon-gray.png" | prepend: "assets/img/" | relative_url }}'   // gray when single column
+            : '{{ "3column-icon-black.png" | prepend: "assets/img/" | relative_url }}'; // black when 3-column
+    });
+});
